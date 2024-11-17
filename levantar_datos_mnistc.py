b2611fe56c260c2e306bee3a7012f40ffa67a5a5
milena-amaro <milenaamaro3@gmail.com>
2024-11-17 17:12:02 -0300

    Update levantar_datos_mnistc.py

--- levantar_datos_mnistc.py
+++ levantar_datos_mnistc.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 13 16:24:31 2024
+
+@author: milen
+"""
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -15,7 +22,7 @@ import numpy as np
 
 
 #%% Cargar datos
-carpeta = "/home/Estudiante/Descargas/Archivos TP2-20241113/"
+carpeta = "C:\\Users\\milen\\OneDrive\\Documents\\Trabajos 2024\\Facultad\\Laboratorio de Datos\\tp2\\"
 # un array para las imágenes, otro para las etiquetas (por qué no lo ponen en el mismo array #$%@*)
 data_imgs = np.load(carpeta + 'mnistc_images.npy')
 data_chrs = np.load(carpeta + 'mnistc_labels.npy')[:,np.newaxis]
@@ -86,6 +93,34 @@ promedio_7 = matriz_de_promedios(matrices_7)
 
 matrices_8 = obtener_matrices_etiquetadas_n(8)
 promedio_8 = matriz_de_promedios(matrices_8)
+
+matrices_9 = obtener_matrices_etiquetadas_n(9)
+promedio_9 = matriz_de_promedios(matrices_9)
+
+#%%
+
+umbral = 5
+
+def matriz_pixeles_iguales(matrices):
+    matriz_resultado = np.zeros((28, 28))
+    for matriz1 in matrices:
+        for matriz2 in matrices:
+            for i in range (0,28):
+                for j in range (0,28):
+                    valor_res = matriz_resultado[i,j]
+                    valor1 = matriz1[i,j]
+                    valor2 = matriz2[i,j]
+                    matriz_resultado[i,j] = valor_res + comparar_pixel(valor1,valor2)
+    return matriz_resultado
+
+def comparar_pixel(valor1, valor2):
+    if (abs(valor1 - valor2) < umbral):
+        return 1 
+    else:
+        return 0
+
+lista_matrices = [promedio_0,promedio_1, promedio_2, promedio_3, promedio_4, promedio_5, promedio_6, promedio_7, promedio_8, promedio_9]
+resultado = matriz_pixeles_iguales(lista_matrices)
 
 matrices_9 = obtener_matrices_etiquetadas_n(9)
 promedio_9 = matriz_de_promedios(matrices_9)
